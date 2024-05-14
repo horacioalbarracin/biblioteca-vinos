d8411fca41c36ccd5e8d63799a071fe68f0d3c72
Horacio <horacio.albarracin@gmail.com>
2024-05-14 19:20:15 -0300

    Consulta
    Se agrega pagina de contacto/consulta

--- static/Js/registro.js
+++ static/Js/registro.js
@@ -2,10 +2,11 @@ document.addEventListener('DOMContentLoaded', function() {
     var formRegister = document.getElementById('formRegister');
     var firstNameInput = document.getElementById('firstname');
     var lastNameInput = document.getElementById('lastname');
-
+    var emailInput = document.getElementById('email');
     formRegister.addEventListener('submit', function(event) {
         var errorFirstName = document.getElementById('error-firstname');
         var errorLastName = document.getElementById('error-lastname');
+        var errorEmail = document.getElementById('error-email');
 
         if (firstNameInput.value.trim() === '') {
             errorFirstName.textContent = 'Falta ingresar el nombre';
@@ -28,5 +29,30 @@ document.addEventListener('DOMContentLoaded', function() {
             errorLastName.classList.remove('error-text'); // Remueve la clase de texto de error
             lastNameInput.classList.remove('input-error', 'lastname'); // Remueve la clase de error si el campo no está vacío
         }
+
+        if (emailInput.value.trim() === '') {
+            errorEmail.textContent = 'Falta ingresar el correo electrónico';
+            errorEmail.classList.add('error-text'); // Agrega la clase de texto de error
+            emailInput.classList.add('input-error', 'email'); // Agrega la clase de error al campo de apellido
+            event.preventDefault(); // Evita que el formulario se envíe
+        }else if (validateEmail(emailInput.value.trim()) == false) {
+            errorEmail.textContent = 'Formato del correo electrónico incorrecto';
+            errorEmail.classList.add('error-text'); // Agrega la clase de texto de error
+            emailInput.classList.add('input-error', 'email'); // Agrega la clase de error al campo de apellido
+            event.preventDefault(); // Evita que el formulario se envíe
+        } else {
+            errorEmail.textContent = ''; // Borra el mensaje de error si el campo no está vacío
+            errorEmail.classList.remove('error-text'); // Remueve la clase de texto de error
+            emailInput.classList.remove('input-error', 'email'); // Remueve la clase de error si el campo no está vacío
+        }
     });
 });
+
+function validateEmail(email){
+	var formatoEmail =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+    if( formatoEmail.test(email) ){
+	    return true;
+    }else{
+		return false;
+    }
+}
